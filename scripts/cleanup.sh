c668a2baa1f4ac99ddd7ae04000ac9eb4bdfa6d8
Jesús Macias Portela <jesus.maciasportela@telefonica.com>
2020-04-06 23:18:10 +0200

    Add confirm force to ansible uninstall

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -8,7 +8,7 @@ apt update
 rm -f /home/sysadmin/*.sh
 
 # Delete Ansible
-pip3 uninstall ansible
+pip3 uninstall -y ansible
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
