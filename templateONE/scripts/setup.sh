7281798a8eac3b5779c30130e6c7920cca1324bd
Jesús Macias Portela <jesus.maciasportela@telefonica.com>
2020-04-07 11:58:03 +0200

    Delete template Folder

--- templateONE/scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-# Add sysadmin user to sudoers.
-echo "sysadmin        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-
-# Disable daily apt unattended updates.
-echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
